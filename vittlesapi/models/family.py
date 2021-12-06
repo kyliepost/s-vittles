65a629149ce115918662e8722734c01b13e21844
kyliepost <kylie.postlethwaite00@gmail.com>
2021-12-06 14:47:41 -0600

    seed the db

--- vittlesapi/models/family.py
+++ vittlesapi/models/family.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Family(models.Model):
     
     name = models.CharField(max_length=55)
     bio = models.TextField()
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
--- vittlesapi/models/recipe.py
+++ vittlesapi/models/recipe.py
@@ -3,4 +3,5 @@ from django.db import models
 class Recipe(models.Model):
     
     name = models.CharField(max_length=55)
+    ingredients = models.TextField()
     description = models.TextField()
