f5e372d740f2d76e082ae19b6520fe47fe05a95b
kyliepost <kylie.postlethwaite00@gmail.com>
2021-12-08 10:44:05 -0600

    server CREATES family

--- vittlesapi/models/family.py
+++ vittlesapi/models/family.py
@@ -5,4 +5,4 @@ class Family(models.Model):
     
     name = models.CharField(max_length=55)
     bio = models.TextField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- vittlesapi/views/family.py
+++ vittlesapi/views/family.py
@@ -1,11 +1,11 @@
-"""View module for handling requests about game types"""
+from django.contrib.auth.models import User
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from django.core.exceptions import ValidationError
-from vittlesapi.models import Family
+from vittlesapi.models import Family, family
 
 
 class FamilyView(ViewSet):
@@ -29,29 +29,17 @@ class FamilyView(ViewSet):
     
     
     def create(self, request):
-        """Handle POST operations
-        Returns:
-            Response -- JSON serialized game instance
-        """
-
-        gamer = Gamer.objects.get(user=request.auth.user)
-
-        # Use the Django ORM to get the record from the database
-        # whose `id` is what the client passed as the
-        game = Game.objects.get(pk=request.data["gameId"])
-
-
-        try:
-            event = Event.objects.create(
-                name=request.data["name"],
-                bio=request.data["bio"],
-                user=user
+        
+        try:               
+            family = Family.objects.create(
+                user = request.auth.user,
+                name = request.data["name"],
+                bio = request.data['bio']
             )
-            serializer = FamilySerializer(event, context={'request': request})
-            return Response(serializer.data)
-
+            family_serializer = FamilySerializer(family, context={'request': request})
+            return Response(family_serializer.data, status=status.HTTP_201_CREATED)
         except ValidationError as ex:
-            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FamilySerializer(serializers.ModelSerializer):
