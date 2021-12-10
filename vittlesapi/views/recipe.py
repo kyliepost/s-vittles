11ebd1246ec4db2c6818be47b9bf9892b1c1bbe8
kyliepost <kylie.postlethwaite00@gmail.com>
2021-12-10 10:49:58 -0600

    trying to expose tags

--- vittlesapi/views/recipe.py
+++ vittlesapi/views/recipe.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from django.core.exceptions import ValidationError
-from vittlesapi.models import Recipe, Tag
+from vittlesapi.models import Recipe, Tag, recipe
 
 
 class RecipeView(ViewSet):
@@ -25,6 +25,10 @@ class RecipeView(ViewSet):
     def list(self, request):
 
         recipes = Recipe.objects.all()
+        
+        tag = self.request.query_params.get('type', None)
+        if tag is not None:
+            recipes = recipes.filter(tag__id=tag)
 
         serializer = RecipeSerializer(
             recipes, many=True, context={'request': request})
@@ -33,11 +37,14 @@ class RecipeView(ViewSet):
     
     def create(self, request):
         
+        tag = Tag.objects.get(pk=request.data['tagId'])
+        
         try:               
             recipe = Recipe.objects.create(
                 name = request.data["name"],
                 ingredients = request.data["ingredients"],
-                description = request.data["description"]
+                description = request.data["description"],
+                tag = tag
             )
             recipe_serializer = RecipeSerializer(recipe, context={'request': request})
             return Response(recipe_serializer.data, status=status.HTTP_201_CREATED)
@@ -57,15 +64,35 @@ class RecipeView(ViewSet):
 
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def update(self, request,pk):
+        try:
+            recipe = Recipe.objects.get(user=request.auth.user, pk=pk)
+            user = User.objects.get(user=request.auth.user)
+            recipe.app_user=user
+            recipe.name = request.data["name"],
+            recipe.ingredients = request.data['ingredients'],
+            recipe.description = request.data['description'],
+            
+            tag = Tag.objects.get(pk=request.data['tagId'])
+            recipe.tag = tag
+            recipe.save()
+            return Response({"Message", "Recipe Updated"}, status=status.HTTP_204_NO_CONTENT)
+        except Recipe.DoesNotExist as ex:
+            return Response({"Message", ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({"Message", ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class RecipeTagSerializer(serializers.ModelSerializer):
+
+# class RecipeTagSerializer(serializers.ModelSerializer):
     
-    class Meta:
-        model = Tag
-        fields = ('id','description')
+#     class Meta:
+#         model = Tag
+#         fields = ('id','description')
 
 class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('id', 'name', 'ingredients', 'description')
+        fields = ('id', 'name', 'ingredients', 'description', 'tag')
+        depth = 1
